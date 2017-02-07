d454d41fc6f5f23eaf997212f214da26fe10aa73
anusha <anushab@outlook.com>
2017-02-06 18:54:37 -0800

    Display list of resources under each subject

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -10,7 +10,9 @@ export default class Section extends Component {
             <div>
                 <h2>{this.props.items.subject}</h2>
                 <ul>
-                    <li>{this.props.items.resources[0].title}</li>
+                    {this.props.items.resources.map(resource => {
+                        return (<li><a href={resource.url}>{resource.title}</a></li>);
+                    })}
                 </ul>
             </div>
             );
